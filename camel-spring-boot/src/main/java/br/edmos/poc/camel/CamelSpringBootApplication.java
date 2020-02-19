9edf867004ea44cc96eb01a3495c373edba9f988
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-19 10:17:45 -0300

    application.properties setup

--- camel-spring-boot/src/main/java/br/edmos/poc/camel/CamelSpringBootApplication.java
+++ camel-spring-boot/src/main/java/br/edmos/poc/camel/CamelSpringBootApplication.java
@@ -1,10 +1,8 @@
 package br.edmos.poc.camel;
 
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 @SpringBootApplication
 public class CamelSpringBootApplication {
@@ -16,9 +14,9 @@ public class CamelSpringBootApplication {
 		SpringApplication.run(CamelSpringBootApplication.class, args);
 	}
 
-	protected ServletRegistrationBean servletRegistrationBean() {
-		ServletRegistrationBean servlet = new ServletRegistrationBean (new CamelHttpTransportServlet(), contextPath + "/*");
-		servlet.setName("CamelServlet");
-		return servlet;
-	}
+//	protected ServletRegistrationBean servletRegistrationBean() {
+//		ServletRegistrationBean servlet = new ServletRegistrationBean (new CamelHttpTransportServlet(), contextPath + "/*");
+//		servlet.setName("CamelServlet");
+//		return servlet;
+//	}
 }
