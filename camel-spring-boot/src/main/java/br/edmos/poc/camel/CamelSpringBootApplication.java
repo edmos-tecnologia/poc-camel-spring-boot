0f3ec7dd53eb5fe0d9b3c62f5ea319cc63dbaa12
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-19 10:33:20 -0300

    application.yml

--- camel-spring-boot/src/main/java/br/edmos/poc/camel/CamelSpringBootApplication.java
+++ camel-spring-boot/src/main/java/br/edmos/poc/camel/CamelSpringBootApplication.java
@@ -1,22 +1,24 @@
 package br.edmos.poc.camel;
 
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 @SpringBootApplication
 public class CamelSpringBootApplication {
 
-	@Value("${poc-camel.api.path}")
+	@Value("${baeldung.api.path}")
 	String contextPath;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamelSpringBootApplication.class, args);
 	}
 
-//	protected ServletRegistrationBean servletRegistrationBean() {
-//		ServletRegistrationBean servlet = new ServletRegistrationBean (new CamelHttpTransportServlet(), contextPath + "/*");
-//		servlet.setName("CamelServlet");
-//		return servlet;
-//	}
+	protected ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean (new CamelHttpTransportServlet(), contextPath + "/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+	}
 }
