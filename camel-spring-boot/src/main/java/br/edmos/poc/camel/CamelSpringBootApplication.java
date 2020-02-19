294658074c171e3c29ee9f564f6e6db44cfbfd77
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-19 09:32:36 -0300

    Camel Spring Boot Application

---
+++ camel-spring-boot/src/main/java/br/edmos/poc/camel/CamelSpringBootApplication.java
@@ -0,0 +1,11 @@
+package br.edmos.poc.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSpringBootApplication.class, args);
+	}
+}
